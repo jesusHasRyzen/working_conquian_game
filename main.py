ca649db315d252db0a8db1599c018ca464d79161
Jesus Ponce <jesus.tony.ponce@gmail.com>
2021-11-15 22:08:01 -0800

    added classes for the game play

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import base64
 
+from random import random
+
+
 import requests
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
@@ -15,9 +18,7 @@ main = Flask(__name__)
 uname = ""
 images = ""
 
-if __name__ == '__main__':
-    # webSite.debug = True
-    main.run()
+
 
 @main.route('/')
 def instructions():
@@ -59,3 +60,87 @@ def instructions2():
 #     # returnFromRequest = requests.get(urlEncrypt)
 #     # encryptPW = decode_to_string(returnFromRequest)
 #     return requests.get(urlImages).content
+
+# class for settings in place to initiate the game
+# such as knowing how many players and cards to deal
+class Settings(object):
+    def __init__(self, numOfPlayers, numOfCards):
+        self.numberOfPlayers = numOfPlayers
+        self.numberOfCards = numOfCards
+
+
+# class Game(object):
+#     def __init__(self, Deck, Settings):
+
+
+# class for cards created via the deck Class
+# current functions include creating new deck
+# shuffle deck and print the deck
+Suits = ["Clubs", "Spades", "Hearts", "Diamonds"]
+Values = ["Ace", 2, 3, 4, 5, 6, 7, "Jack", "Queen", "King"]
+class Card(object):
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+    def printCard(self):
+        print("{} of {}".format(self.value, self.suit))
+class Deck(object):
+    def __init__(self):
+        self.cards = []
+        self.newDeck()
+        self.shuffleDeck()
+
+    def newDeck(self):
+        for suit in Suits:
+            for val in Values:
+                self.cards.append(Card(val, suit))
+
+    def shuffleDeck(self):
+        numOfCards = 40
+        for i in range(numOfCards):
+            j = int(random() * i)
+            self.cards[i], self.cards[j] = self.cards[j], self.cards[i]
+
+
+    def printDeck(self):
+        for card in self.cards:
+            card.printCard()
+
+# class for the player where it initiates the player already having his
+# hand and the options the player has such as switching cards to start
+# the game, draw card, playcard and discard card
+class Player(object):
+    def __init__(self):
+        self.hand = []
+    # def switchCard(self):
+    #
+    # def drawACard(self, deck):
+    # if needed :
+    #     putDownSequence()
+    # else:
+    #     discardCard()
+    # def passOnCard(self):
+    #
+    # def pickUpCard(self):
+    #     playCardPickedUp()
+    #
+    # def discardCard(self):
+    #
+    # def playCardPickedUp(self):
+    #     putDownSequence()
+    #     discardCard()
+    #
+    # def putDownSequence(self):
+
+
+
+
+
+
+
+
+myDeck = Deck()
+myDeck.printDeck()
+if __name__ == '__main__':
+    # webSite.debug = True
+    main.run()
